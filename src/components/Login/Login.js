1edfdb21c89e731ba1fa5256aea534a8fbc1f0b1
Saqib Ahmad <you@example.com>
2022-10-25 14:16:17 +0600

    registration page impleted

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,16 +15,14 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
+                                <input type="text" name="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-
-                                <span className='label-text-alt'>Already have an account? <Link to='/signup' className="label-text-alt link link-hover">Sign up now</Link></span>
-
+                                <input type="text" name="password" placeholder="password" className="input input-bordered" />
+                                <span className='label-text-alt'>Don't have an account? <Link to='/signup' className="label-text-alt link link-hover">Sign up now</Link></span>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
--- src/components/SignuUp/SignUp.js
+++ src/components/SignuUp/SignUp.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
         <div>
-            
+            <div>
+                <div className="hero min-h-screen bg-base-200">
+                    <div className="hero-content flex-col ">
+                        <div className="text-center">
+                            <h1 className="text-5xl font-bold">SIGN UP FOR FREE!</h1>
+                        </div>
+                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <form className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Full Name</span>
+                                    </label>
+                                    <input type="text" name="name" placeholder="your full name" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo URL</span>
+                                    </label>
+                                    <input type="text" name="photoURL" placeholder="your photo URL" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="text" name="password" placeholder="password" className="input input-bordered" />
+                                    <span className='label-text-alt'>Already have an account? <Link to='/login' className="label-text-alt link link-hover">Login</Link></span>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Sign up</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
